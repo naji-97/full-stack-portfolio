89966d0369a0403177942741d86d4155ac37946c
naji-97 <elbairabennaji@gmail.com>
2025-09-11 18:42:19 +0100

    update work project section

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -19,20 +19,23 @@ const projects = [
   {
     num: "01",
     category: "Fullstack",
-    title: "Inventory Management System",
+    title: "Project Management System",
     description:
-      "A full-stack inventory management dashboard using Next.js, Redux Toolkit, Node.js, and AWS. Features include product management, data visualization, and seamless API integration with a PostgreSQL database.",
+      "A full‑stack Project Management Dashboard built with Next.js, Node.js, and AWS Services. This application helps teams organize, track, and collaborate on projects more efficiently ",
     stack: [
       { name: "Next.js" },
       { name: "React" },
+      { name: "Material UI" },
+      { name: "Tailwind CSS" },
       { name: "Redux Toolkit" },
       { name: "Node.js" },
       { name: "Prisma" },
-      { name: "AWS" }
+      { name: "AWS" },
+      { name: "Cognito" }
     ],
-    image: "/assets/work/inventory-management.jpeg",
-    live: "https://main.d1fuz2wu7nv9rz.amplifyapp.com/dashboard",
-    github: "https://github.com/naji-97/Inventory-Management"
+    image: "/assets/work/project-management.jpeg",
+    live: "https://main.d2rkgcgqjm6o35.amplifyapp.com",
+    github: "https://github.com/naji-97/project-managment"
   },
   {
     num: "02",
@@ -54,7 +57,7 @@ const projects = [
     num: "03",
     category: "frontend",
     title: "We Are Developer",
-    description:"The website for the world's leading developer event is designed using modern CSS techniques like CSS Grid, Flexbox, and pseudo-selectors.",
+    description: "The website for the world's leading developer event is designed using modern CSS techniques like CSS Grid, Flexbox, and pseudo-selectors.",
     stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }, { name: "Github" }, { name: "Webpack" }],
     image: "/assets/work/project1.png",
     live: "https://naji-97.github.io/CapstonePrj1-ConferencePage/index.html",
@@ -71,8 +74,8 @@ const projects = [
     live: "https://spacehub.onrender.com/",
     github: "https://github.com/naji-97/sec-space-travels-hub"
   },
-  
-  
+
+
 
 ]
 const Work = () => {
@@ -81,8 +84,8 @@ const Work = () => {
     setProject(projects[swiper.activeIndex]);
   }
 
-  return <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 ,transition: { duration: 0.4, delay: 2.4, ease: "easeIn" } }}
-    className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+  return <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.4, delay: 2.4, ease: "easeIn" } }}
+    className="h-full flex flex-col justify-center py-12 xl:px-0">
     <div className="container mx-auto">
       <div className="flex flex-col xl:flex-row xl:gap-[30px]">
         <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
@@ -96,9 +99,9 @@ const Work = () => {
             {/* Project description */}
             <p className="text-white/60 ">{project.description} </p>
             {/* Project stack */}
-            <ul className="flex gap-3  ">
+            <ul className="flex flex-wrap gap-2 ">
               {project.stack.map((item, index) => (
-                <li key={index} className=" inline text-xl text-accent  font-medium whitespace-nowrap">
+                <li key={index} className=" text-xl text-accent  font-medium ">
                   {item.name}{index < project.stack.length - 1 ? "," : ""}
                 </li>
               ))}
@@ -150,23 +153,23 @@ const Work = () => {
                   {/* Image */}
                   <div className="relative w-full h-full ">
 
-                  <Image
-                    src={item.image}
-                    alt={item.title}
-                    fill
-                    
+                    <Image
+                      src={item.image}
+                      alt={item.title}
+                      fill
+
                       className="w-full h-full object-contain group-hover:scale-105 transition-all duration-500 "
-                  />
+                    />
                   </div>
                 </div>
-                
+
 
               </SwiperSlide>
             ))}
             {/* Slider Buttons */}
-            <WorkdSilderButtons containerStyles="flex gap-6 absolute right-0  bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-center  " 
-            btnSyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex items-center justify-center transition-all duration-500 cursor-pointer rounded-full" 
-            iconsStyles="" />
+            <WorkdSilderButtons containerStyles="flex gap-6 absolute right-0  bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-center  "
+              btnSyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex items-center justify-center transition-all duration-500 cursor-pointer rounded-full"
+              iconsStyles="" />
           </Swiper>
         </div>
 
--- components/WorkdSilderButtons.tsx
+++ components/WorkdSilderButtons.tsx
@@ -9,13 +9,32 @@ interface Props {
 }
 const WorkdSilderButtons = ({ containerStyles, btnSyles, iconsStyles }: Props) => {
     const swiper = useSwiper();
+    const handlePrevSlide = (swiper: any) => {
+        // You can access the current slide index using swiper.activeIndex
+        console.log('Current slide index:', swiper.activeIndex);
+        if(swiper.activeIndex === 0){
+            swiper.slideTo(swiper.slides.length - 1); // Go to the last slide
+        } else {
+            swiper.slidePrev(); // Go to the previous slide
+        }
+
+    }
+    const handleNextSlide = (swiper: any) => {
+        // You can access the current slide index using swiper.activeIndex
+        console.log('Current slide index:', swiper.activeIndex);
+        if(swiper.activeIndex === swiper.slides.length - 1){
+            swiper.slideTo(0); // Go to the first slide
+        } else {
+            swiper.slideNext(); // Go to the next slide
+        }
+    }
   return (
     <div className={containerStyles}>
       <button
         className={btnSyles}
         title="Previous Slide"
         aria-label="Previous Slide"
-        onClick={() => swiper.slidePrev()}
+        onClick={ () => handlePrevSlide(swiper)}
       >
         <PiCaretLeftBold className={iconsStyles} />
       </button>
@@ -23,7 +42,7 @@ const WorkdSilderButtons = ({ containerStyles, btnSyles, iconsStyles }: Props) =
         className={btnSyles}
         title="Next Slide"
         aria-label="Next Slide"
-        onClick={() => swiper.slideNext()}
+        onClick={() => handleNextSlide(swiper)}
       >
         <PiCaretRightBold  className={iconsStyles} />
       </button>
